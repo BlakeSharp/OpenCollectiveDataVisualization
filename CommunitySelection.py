013ea8ec6f2656977e3e74b5c85edb65108e3b1d
BlakeSharp <blake.asher.sharp@gmail.com>
2021-12-28 21:42:35 -0600

    Finished Selection Screen

--- CommunitySelection.py
+++ CommunitySelection.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import Label
+from tkinter import Label, messagebox
 import requests
 import csv
 #this is the url for webpack which we should change with the thing the user wants
@@ -17,13 +17,13 @@ canvas1.pack()
 NameEntry = tk.Entry (root) 
 canvas1.create_window(200, 140, window=NameEntry)
 def errorLabel():
-    error = Label(canvas1, text = "Error Please Try Again")
-    error.pack()
+    messagebox.showerror('Error','There was an error requesting data. Please check spelling and try again.')
 
     
 
 def getFile ():  
     Name = NameEntry.get()
+    Name = Name.strip().lower()
     url = 'https://rest.opencollective.com/v2/{}/transactions.txt?kind=CONTRIBUTION%2CEXPENSE%2CHOST_FEE%2CPAYMENT_PROCESSOR_COVER&includeGiftCardTransactions=1&includeIncognitoTransactions=1&includeChildrenTransactions=1'.format(Name)
     r = requests.get(url, allow_redirects=True)
     open('Data.csv', 'wb').write(r.content)
