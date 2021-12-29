2c18d509d2fe5bf99102b446c27f65dafc50e2c4
BlakeSharp <blake.asher.sharp@gmail.com>
2021-12-28 21:36:31 -0600

    first commit

---
+++ SelectionScreen.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import Label
+import requests
+import csv
+#this is the url for webpack which we should change with the thing the user wants
+#https://rest.opencollective.com/v2/webpack/transactions.txt?kind=CONTRIBUTION%2CEXPENSE%2CHOST_FEE%2CPAYMENT_PROCESSOR_COVER&includeGiftCardTransactions=1&includeIncognitoTransactions=1&includeChildrenTransactions=1
+
+
+root= tk.Tk()
+root.geometry('500x300')
+root.resizable(False,False)
+root.title('Open Collective Visualization')
+
+canvas1 = tk.Canvas(root, width = 400, height = 300)
+canvas1.pack()
+
+NameEntry = tk.Entry (root) 
+canvas1.create_window(200, 140, window=NameEntry)
+def errorLabel():
+    error = Label(canvas1, text = "Error Please Try Again")
+    error.pack()
+
+    
+
+def getFile ():  
+    Name = NameEntry.get()
+    url = 'https://rest.opencollective.com/v2/{}/transactions.txt?kind=CONTRIBUTION%2CEXPENSE%2CHOST_FEE%2CPAYMENT_PROCESSOR_COVER&includeGiftCardTransactions=1&includeIncognitoTransactions=1&includeChildrenTransactions=1'.format(Name)
+    r = requests.get(url, allow_redirects=True)
+    open('Data.csv', 'wb').write(r.content)
+    r.close() 
+    file = open('Data.csv').readlines()
+    if(str(file[0])[:10]!='"datetime"'):
+        errorLabel()
+    
+findDataButton = tk.Button(text='Find', command=getFile)
+canvas1.create_window(200, 180, window=findDataButton)
+
+root.mainloop()
