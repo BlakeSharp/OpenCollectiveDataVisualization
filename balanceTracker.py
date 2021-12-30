c462556d077a39eadae34ed697bef11e6b279b14
BlakeSharp <blake.asher.sharp@gmail.com>
2021-12-30 16:52:07 -0600

    successfully added the balance over time graph

--- balanceTracker.py
+++ balanceTracker.py
@@ -1,10 +1,31 @@
 import pandas as pd
+import plotly.graph_objects as go
 
+data = pd.read_csv('Community/Data.csv')
 def balanceMain():
-    data = pd.read_csv('Community/Data.csv')
-    for elem in data['Transaction Description']:
-        if('contribution' not in elem):
-            print(elem)
-    print('running balance')
+    data['Net Amount (USD)'] = round(data['Net Amount (USD)'].astype(float),2)
+    for x in range(0, len(data['Transaction Description'])):
+        if('contribution' not in data['Transaction Description'][x]):
+            print(data['Transaction Description'][x] )
+            print(data['Transaction Amount'][x])
+            print(data['Net Amount (USD)'][x])
+            print(data['User Name'][x])
+            print("----------------------")
+    data['Running Balance'] = data['Net Amount (USD)'][len(data['Net Amount (USD)'])-2]
+    for x in reversed(range(0,(len(data['Net Amount (USD)']))-2)):
+        data['Running Balance'][x] = (data['Net Amount (USD)'][x] + data['Running Balance'][x+1])
+    data.to_csv("Data.csv", index=False)
+    creatingGraph()
 
-balanceMain()
+
+
+def creatingGraph():
+    fig = go.Figure([go.Scatter(x=data['Transaction Date'], y=data['Running Balance'])])
+    fig.show()
+
+
+def main():
+    balanceMain()
+
+if __name__ == '__main__':
+    main()
