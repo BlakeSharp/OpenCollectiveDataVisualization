923fbd4c86b0a748cfbdd111faa6a382bcc65514
BlakeSharp <blake.asher.sharp@gmail.com>
2022-01-05 12:43:07 -0600

    Grouped each days net contribution so now it gives a better representation of a pattern (if any)

--- ContributionPerDay.py
+++ ContributionPerDay.py
@@ -1,17 +1,26 @@
 import plotly.express as px
 import pandas as pd
+import datetime
 
 data = pd.read_csv('Community/Data.csv')
 data['Net Amount (USD)'] = round(data['Net Amount (USD)'].astype(float),2)
 edit = data.loc[data['Net Amount (USD)'] > 0]
-
+for i in range(0,len(data['Order Date'])):
+    try:
+        data['Order Date'][i] = datetime.datetime.strptime(data['Order Date'][i].split('T')[0], "%Y-%m-%d")
+    except:
+        pass
 
 def contributionMain():
+    data.to_csv("Community/Data.csv", index=False)
     createGraph()
 
 def createGraph():
-    fig = px.line(edit, x=edit['Transaction Date'], y=edit['Net Amount (USD)'])
+    fig = px.line(edit, x=edit['Order Date'], y =edit['Net Amount (USD)'])
     fig.show()
+
+
+# this stops the import from running the script and allows from running direct from this file 
 
 def main():
     contributionMain()
