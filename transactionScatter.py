e04a79274727c29eaf7d3a625c74b1079bb41bdd
BlakeSharp <blake.asher.sharp@gmail.com>
2022-01-09 22:27:06 -0600

    Changed the color indentifier  to absolute value to prepare for the merge of contribution and expenses

--- transactionScatter.py
+++ transactionScatter.py
@@ -2,38 +2,62 @@ import plotly.express as px
 import pandas as pd
 import datetime
 
-data = pd.read_csv('Community/Data.csv')
-data['Net Amount (USD)'] = round(data['Net Amount (USD)'].astype(float),2)
-editpos = data.loc[data['Net Amount (USD)'] > 0]
-editneg = data.loc[data['Net Amount (USD)'] < 0]
+data = pd.read_csv("Community/Data.csv")
+data["Net Amount (USD)"] = round(data["Net Amount (USD)"].astype(float), 2)
+editpos = data.loc[data["Net Amount (USD)"] > 0]
+editneg = data.loc[data["Net Amount (USD)"] < 0]
 
-for i in range(0,len(editpos['Order Date'])):
+for i in range(0, len(editpos["Order Date"])):
     try:
-        editpos['Order Date'][i] = datetime.datetime.strptime(editpos['Order Date'][i].split('T')[0], "%Y-%m-%d")
+        editpos["Order Date"][i] = datetime.datetime.strptime(
+            editpos["Order Date"][i].split("T")[0], "%Y-%m-%d"
+        )
     except:
         pass
 
-for i in range(0,len(editneg['Order Date'])):
+for i in range(0, len(editneg["Order Date"])):
     try:
-        editneg['Order Date'][i] = datetime.datetime.strptime(editneg['Order Date'][i].split('T')[0], "%Y-%m-%d")
+        editneg["Order Date"][i] = datetime.datetime.strptime(
+            editneg["Order Date"][i].split("T")[0], "%Y-%m-%d"
+        )
     except:
         pass
+
 
 def contributionMain():
     createGraph()
 
+
 def createGraph():
-    fig = px.scatter(editpos, x=editpos['Order Date'], y =(editpos['Net Amount (USD)']), hover_data=[editpos['User Name'],editpos['Transaction Description']], title="Net contributions per day (USD) over time", color=editpos['Net Amount (USD)'],range_color=[0, 1000])
-    fig1 = px.scatter(editneg, x=editneg['Order Date'], y =(editneg['Net Amount (USD)']), hover_data=[editneg['User Name'],editneg['Transaction Description']], title="Net contributions per day (USD) over time", color=editneg['Net Amount (USD)'],range_color=[-10000, 0])
+    fig = px.scatter(
+        editpos,
+        x=editpos["Order Date"],
+        y=(editpos["Net Amount (USD)"]),
+        hover_data=[editpos["User Name"], editpos["Transaction Description"]],
+        title="Net contributions per day (USD) over time",
+        color=editpos["Net Amount (USD)"],
+        range_color=[0, 1000],
+    )
+    fig1 = px.scatter(
+        editneg,
+        x=editneg["Order Date"],
+        y=(editneg["Net Amount (USD)"]),
+        hover_data=[editneg["User Name"], editneg["Transaction Description"]],
+        title="Net contributions per day (USD) over time",
+        color=abs(editneg["Net Amount (USD)"]),
+        range_color=[0, 5000],
+    )
     fig.update_layout(hovermode="x unified")
     fig1.show()
     fig.show()
 
 
-# this stops the import from running the script and allows from running direct from this file 
+# this stops the import from running the script and allows from running direct from this file
+
 
 def main():
     contributionMain()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
