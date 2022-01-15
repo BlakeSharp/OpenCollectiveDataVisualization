944abe3ce31c89b34301aa9333022f4b58e89084
BlakeSharp <blake.asher.sharp@gmail.com>
2022-01-14 23:28:48 -0600

    formatting

--- ContributionPerDay.py
+++ ContributionPerDay.py
@@ -2,30 +2,42 @@ import plotly.express as px
 import pandas as pd
 import datetime
 
-data = pd.read_csv('Community/Data.csv')
-data['Net Amount (USD)'] = round(data['Net Amount (USD)'].astype(float),2)
-edit = data.loc[data['Net Amount (USD)'] > 0]
+data = pd.read_csv("Community/Data.csv")
+data["Net Amount (USD)"] = round(data["Net Amount (USD)"].astype(float), 2)
+edit = data.loc[data["Net Amount (USD)"] > 0]
 
-for i in range(0,len(data['Order Date'])):
+for i in range(0, len(data["Order Date"])):
     try:
-        data['Order Date'][i] = datetime.datetime.strptime(data['Order Date'][i].split('T')[0], "%Y-%m-%d")
+        data["Order Date"][i] = datetime.datetime.strptime(
+            data["Order Date"][i].split("T")[0], "%Y-%m-%d"
+        )
     except:
         pass
 data.to_csv("Community/Data.csv", index=False)
 
+
 def contributionMain():
     createGraph()
 
+
 def createGraph():
-    fig = px.line(edit, x=edit['Order Date'], y =(edit['Net Amount (USD)']), hover_data=[edit['User Name'],edit['Transaction Description']], title="Net contributions per day (USD) over time")
+    fig = px.line(
+        edit,
+        x=edit["Order Date"],
+        y=(edit["Net Amount (USD)"]),
+        hover_data=[edit["User Name"], edit["Transaction Description"]],
+        title="Net contributions per day (USD) over time",
+    )
     fig.update_layout(hovermode="x unified")
     fig.show()
 
 
-# this stops the import from running the script and allows from running direct from this file 
+# this stops the import from running the script and allows from running direct from this file
+
 
 def main():
     contributionMain()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- expensePerDay.py
+++ expensePerDay.py
@@ -2,31 +2,43 @@ import plotly.express as px
 import pandas as pd
 import datetime
 
-data = pd.read_csv('Community/Data.csv')
-data['UserName'] = ''
-    
-data['Net Amount (USD)'] = round(data['Net Amount (USD)'].astype(float),2)
-edit = data.loc[data['Net Amount (USD)'] < 0]
+data = pd.read_csv("Community/Data.csv")
+data["UserName"] = ""
 
-for i in range(0,len(data['Order Date'])):
+data["Net Amount (USD)"] = round(data["Net Amount (USD)"].astype(float), 2)
+edit = data.loc[data["Net Amount (USD)"] < 0]
+
+for i in range(0, len(data["Order Date"])):
     try:
-        data['Order Date'][i] = datetime.datetime.strptime(data['Order Date'][i].split('T')[0], "%Y-%m-%d")
+        data["Order Date"][i] = datetime.datetime.strptime(
+            data["Order Date"][i].split("T")[0], "%Y-%m-%d"
+        )
     except:
         break
+
 
 def expensesMain():
     data.to_csv("Community/Data.csv", index=False)
     createGraph()
 
+
 def createGraph():
-    fig = px.line(edit, x=edit['Order Date'], y =edit['Net Amount (USD)'], hover_data=[edit['User Name']], title="Net expenses per day (USD) over time")
+    fig = px.line(
+        edit,
+        x=edit["Order Date"],
+        y=edit["Net Amount (USD)"],
+        hover_data=[edit["User Name"]],
+        title="Net expenses per day (USD) over time",
+    )
     fig.show()
 
 
-# this stops the import from running the script and allows from running direct from this file 
+# this stops the import from running the script and allows from running direct from this file
+
 
 def main():
     expensesMain()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
